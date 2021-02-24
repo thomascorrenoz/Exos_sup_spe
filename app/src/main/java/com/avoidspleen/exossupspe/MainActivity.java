d3ed564c61652515f1e83fc1ce86c5c2dcbeaa44
thomascorrenoz <thomas.correnoz@gmail.com>
2021-02-24 11:44:53 +0100

    Action bar added
    - New XML action_bar layout
    - primitive interaction and testing tools

--- app/src/main/java/com/avoidspleen/exossupspe/MainActivity.java
+++ app/src/main/java/com/avoidspleen/exossupspe/MainActivity.java
@@ -3,6 +3,10 @@ package com.avoidspleen.exossupspe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView baseline_menu_action_bar = findViewById(R.id.baseline_menu_icon);
+        TextView title = findViewById(R.id.tile_action_bar);
+
+        baseline_menu_action_bar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Test action bar menu", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
