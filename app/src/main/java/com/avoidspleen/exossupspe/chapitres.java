2868a01a5cbcd90b9857579aa2fe1fdf44f40622
thomascorrenoz <thomas.correnoz@gmail.com>
2021-02-26 00:51:43 +0100

    Nouvelle activité chapitres

---
+++ app/src/main/java/com/avoidspleen/exossupspe/chapitres.java
@@ -0,0 +1,16 @@
+package com.avoidspleen.exossupspe;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/*
+ * Main Activity class that loads {@link MainFragment}.
+ */
+public class chapitres extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chapitres);
+    }
+}
