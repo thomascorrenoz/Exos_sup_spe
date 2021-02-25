75190ec2537b96733f02d3f1328ab44f743138a6
thomascorrenoz <thomas.correnoz@gmail.com>
2021-02-26 00:55:50 +0100

    Ajouté activité scrollable_exercices

---
+++ app/src/main/java/com/avoidspleen/exossupspe/scrollable_exercices.java
@@ -0,0 +1,14 @@
+package com.avoidspleen.exossupspe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class scrollable_exercices extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scrollable_exercices);
+    }
+}
