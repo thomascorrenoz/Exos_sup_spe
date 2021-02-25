13e112007e2275cc8f7c8ca500d04c948bf764cf
thomascorrenoz <thomas.correnoz@gmail.com>
2021-02-26 00:53:31 +0100

    Ajouté activité sous_chapitre

---
+++ app/src/main/java/com/avoidspleen/exossupspe/sous_chapitres.java
@@ -0,0 +1,14 @@
+package com.avoidspleen.exossupspe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sous_chapitres extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sous_chapitres);
+    }
+}
