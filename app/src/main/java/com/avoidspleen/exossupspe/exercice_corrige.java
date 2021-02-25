9d72dc19c7f8baa29a64e81e048c75d1ed183217
thomascorrenoz <thomas.correnoz@gmail.com>
2021-02-26 00:56:34 +0100

    Ajouté activité exercice_corrige

---
+++ app/src/main/java/com/avoidspleen/exossupspe/exercice_corrige.java
@@ -0,0 +1,14 @@
+package com.avoidspleen.exossupspe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class exercice_corrige extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercice_corrige);
+    }
+}
